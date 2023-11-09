3f660d60b951054a34b4fa46655769449b0085be
professorVerbrugghe <lotrdetailsys@gmail.com>
2023-11-09 18:39:31 +0100

    Lesson 1.4 - Step into the Driver's Seat
    Summary
    Overview:
    In this lesson, we need to hit the road and gain control of the vehicle. In order to do so, we need to detect when the player is pressing the arrow keys, then accelerate and turn the vehicle based on that input. Using new methods, Vectors, and variables, you will allow the vehicle to move forwards or backwards and turn left to right.
    Project Outcome:
    When the player presses the up/down arrows, the vehicle will move forward and backward. When the player presses the left/right arrows, the vehicle will turn.

--- Car game/UprjPrototype1/Assets/Scripts/CameraFollow.cs
+++ Car game/UprjPrototype1/Assets/Scripts/CameraFollow.cs
@@ -4,17 +4,15 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    //variable for offset camera
     private Vector3 offset = new Vector3(0, 5, -10);
     public GameObject player;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
 
     // Update is called once per frame
     void LateUpdate()
     {
+        //placing camera
         transform.position = player.transform.position + offset;
     }
 }
--- Car game/UprjPrototype1/Assets/Scripts/PlayerController.cs
+++ Car game/UprjPrototype1/Assets/Scripts/PlayerController.cs
@@ -5,17 +5,23 @@ using UnityEngine.Jobs;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 1.0f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float speed = 20f;
+    private float rotationSpeed = 40f;
+    private float horizontalInput;
+    private float verticalInput;
+
 
     // Update is called once per frame
     void Update()
     {
+        //horizontal and vertical input with input system
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
         //move vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+
+        //rotate vehicle
+        transform.Rotate(Vector3.up, Time.deltaTime * rotationSpeed * horizontalInput);
     }
 }
