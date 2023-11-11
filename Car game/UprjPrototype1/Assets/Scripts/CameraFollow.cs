18893f63d38e4c951e19a4fad97e8a0fcd14aa80
professorVerbrugghe <lotrdetailsys@gmail.com>
2023-11-11 19:13:07 +0100

    Added camera control

--- Car game/UprjPrototype1/Assets/Scripts/CameraFollow.cs
+++ Car game/UprjPrototype1/Assets/Scripts/CameraFollow.cs
@@ -5,13 +5,33 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     //variable for offset camera
-    private Vector3 offset = new Vector3(0, 5, -10);
+    private Vector3 offsetBehind = new Vector3(0, 5, -10);
+    private Vector3 offsetUp = new Vector3(0, 7.5f, -10);
+    private Vector3 offsetDriverSeat = new Vector3(0, 2.5f, 2.5f);
+    private Vector3 offset;
     public GameObject player;
 
+    private void Start() {
+        offset = offsetBehind;
+    }
 
-    // Update is called once per frame
+    void Update() {
+        if(Input.GetKeyDown("f"))
+        {
+            if (offset == offsetBehind) {
+                offset = offsetUp;
+            }
+            else if (offset == offsetUp) {
+                offset = offsetDriverSeat;
+            }
+            else { offset = offsetBehind;}
+        }
+    }
+
     void LateUpdate()
     {
+        
+
         //placing camera
         transform.position = player.transform.position + offset;
     }
