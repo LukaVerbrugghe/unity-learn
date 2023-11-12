22e12f4a623c0be5978084da0f74bc870b372081
professorVerbrugghe <lotrdetailsys@gmail.com>
2023-11-12 19:25:38 +0100

    Added local multiplayer + RELEASE
    Added local multiplayer using two cameras and the integrated input system. Built for windows and webGL. Made an installer.

---
+++ Car game/UprjPrototype1/Assets/Scripts/SecondCameraControler.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondCameraControler : MonoBehaviour
+{
+    //variable for offset camera
+    private Vector3 offsetBehind = new Vector3(0, 5, -10);
+    private Vector3 offsetUp = new Vector3(0, 7.5f, -10);
+    private Vector3 offsetDriverSeat = new Vector3(0, 2.5f, 2.5f);
+    private Vector3 offset;
+    public GameObject player;
+
+    private void Start() {
+        offset = offsetBehind;
+    }
+
+    void Update() {
+        //does the camera needs to move?
+        if (Input.GetKeyDown("f")) {
+            //swithch camera offset
+            if (offset == offsetBehind) {
+                offset = offsetUp;
+            } else if (offset == offsetUp) {
+                offset = offsetDriverSeat;
+            } else { offset = offsetBehind; }
+        }
+    }
+
+    void LateUpdate() {
+        //placing camera
+        transform.position = player.transform.position + offset;
+    }
+}
