eae67d0bab6f889f2d86bb2db615d70abdc30afc
professorVerbrugghe <lotrdetailsys@gmail.com>
2023-11-18 18:33:39 +0100

    Lesson 2.1 - Player Positioning
    Summary
    Overview:
    You will begin this unit by creating a new project for your second Prototype and getting basic player movement working. You will first choose which character you would like, which types of animals you would like to interact with, and which food you would like to feed those animals. You will give the player basic side-to-side movement just like you did in Prototype 1, but then you will use if-then statements to keep the Player in bounds.
    Project Outcome:
    The player will be able to move left and right on the screen based on the user’s left and right key presses, but will not be able to leave the play area on either side.

--- Pizza Thrower/UprjPrototype2/Assets/Scripts/PlayerController.cs
+++ Pizza Thrower/UprjPrototype2/Assets/Scripts/PlayerController.cs
@@ -6,9 +6,16 @@ public class PlayerController : MonoBehaviour
 {
     public float playerHorizontal;
     public float speed = 25.0f;
+    private float xBorder = 11.0f;
 
     void Update()
     {
+        if (transform.position.x < -xBorder) {
+            transform.position = new Vector3(-xBorder,transform.position.y,transform.position.z);
+        }
+        if (transform.position.x > xBorder) {
+            transform.position = new Vector3(xBorder, transform.position.y,transform.position.z);
+        }
         playerHorizontal = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * playerHorizontal * Time.deltaTime * speed);
     }
