3bacd0d6fddf29a0860c568c7e41b721b1e7dbb3
professorVerbrugghe <lotrdetailsys@gmail.com>
2023-11-23 10:10:54 +0100

    Added script for car selection

---
+++ Car game/UprjPrototype1/Assets/Scripts/ChangeCarPlayer.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCarPlayer : MonoBehaviour
+{
+    private void Update()
+    {
+        
+    }
+}
