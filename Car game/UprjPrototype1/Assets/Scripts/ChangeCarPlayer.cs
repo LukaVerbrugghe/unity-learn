cc418c60ccea13b78f35881f3948ae4a55623e97
professorVerbrugghe <lotrdetailsys@gmail.com>
2023-11-24 09:13:27 +0100

    Continued work on car selection

--- Car game/UprjPrototype1/Assets/Scripts/ChangeCarPlayer.cs
+++ Car game/UprjPrototype1/Assets/Scripts/ChangeCarPlayer.cs
@@ -1,11 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ChangeCarPlayer : MonoBehaviour
 {
+    //een reference naar de speler maken
+    public GameObject player1;
+    public GameObject player2;
+
     private void Update()
     {
+        //input vragen
+        if (Input.GetKeyDown("1"))
+        {
+            player1.mesh
+        }
+        else if (Input.GetKeyDown("2"))
+        {
+
+        }
+        else if(Input.GetKeyDown("3")) 
+        {
+           
+        }
+        else if (Input.GetKeyDown("4"))
+        {
+
+        }
+        else if (Input.GetKeyDown("5")) 
+        {
         
+        }
     }
 }
