bc662eeb8fcbf1b1aafdc2a3039f06fd5bb8604b
professorVerbrugghe <lotrdetailsys@gmail.com>
2023-11-24 10:06:07 +0100

    fack dees

--- Car game/UprjPrototype1/Assets/Scripts/ChangeCarPlayer.cs
+++ Car game/UprjPrototype1/Assets/Scripts/ChangeCarPlayer.cs
@@ -9,12 +9,21 @@ public class ChangeCarPlayer : MonoBehaviour
     public GameObject player1;
     public GameObject player2;
 
+    public MeshFilter filterPlayer1;
+    public MeshFilter filterPlayer2;
+
+    public Mesh vehicle1;
+    public Mesh vehicle2;
+    public Mesh vehicle3;
+    public Mesh vehicle4;
+    public Mesh vehicle5;
+
     private void Update()
     {
         //input vragen
         if (Input.GetKeyDown("1"))
         {
-            player1.mesh
+            
         }
         else if (Input.GetKeyDown("2"))
         {
@@ -31,6 +40,10 @@ public class ChangeCarPlayer : MonoBehaviour
         else if (Input.GetKeyDown("5")) 
         {
         
+        }
+        else if (Input.GetKeyDown("6"))
+        {
+
         }
     }
 }
