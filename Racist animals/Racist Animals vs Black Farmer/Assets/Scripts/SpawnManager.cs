1ef117383a2dfef507f8f444a16d95887fdd2fcc
professorVerbrugghe <lotrdetailsys@gmail.com>
2023-11-25 14:22:31 +0100

    prj 2 - tot 4 denk ik

--- Racist animals/Racist Animals vs Black Farmer/Assets/Scripts/SpawnManager.cs
+++ Racist animals/Racist Animals vs Black Farmer/Assets/Scripts/SpawnManager.cs
@@ -6,4 +6,13 @@ public class SpawnManager : MonoBehaviour
 {
     //variables
     public GameObject[] animalPrefabs;
+    public int animalIndex;
+
+    private void Update() {
+        //dier spawnen
+        if (Input.GetKeyDown(KeyCode.S)) { 
+            animalIndex = Random.Range(0, animalPrefabs.Length);
+            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20), animalPrefabs[animalIndex].transform.rotation);
+        }
+    }
 }
