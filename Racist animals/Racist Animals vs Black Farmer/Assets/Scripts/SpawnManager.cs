80e6d5d35bbbcd7aa7d19035e244206856058ed2
professorVerbrugghe <lotrdetailsys@gmail.com>
2023-11-28 20:23:05 +0100

    Started new lesson

--- Racist animals/Racist Animals vs Black Farmer/Assets/Scripts/SpawnManager.cs
+++ Racist animals/Racist Animals vs Black Farmer/Assets/Scripts/SpawnManager.cs
@@ -8,13 +8,20 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] animalPrefabs;
     private int spawnRangeX = 10;
     private int spawnRangeZ = 20;
+    private float startDelay = 2;
+    private float repeatRate = 1.5f;
+
+    private void Start() {
+        InvokeRepeating("SpawnRandomAnimal", startDelay, repeatRate);
+    }
 
     private void Update() {
-        //dier spawnen
-        if (Input.GetKeyDown(KeyCode.S)) { 
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Vector3 spawnPos = new Vector3(Random.RandomRange(-spawnRangeX, spawnRangeX), 0, spawnRangeZ);
-            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
-        }
+       
+    }
+
+    private void SpawnRandomAnimal() {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.RandomRange(-spawnRangeX, spawnRangeX), 0, spawnRangeZ);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
     }
 }
