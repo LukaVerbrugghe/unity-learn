900b50c51c9c6f9096921a0a5ba558f1f2dbba05
professorVerbrugghe <lotrdetailsys@gmail.com>
2023-12-04 20:13:40 +0100

    Finished aggresive animals

--- Racist animals/Racist Animals vs Black Farmer/Assets/Scripts/DestroyOutOfBounds.cs
+++ Racist animals/Racist Animals vs Black Farmer/Assets/Scripts/DestroyOutOfBounds.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class DestroyOutOfBounds : MonoBehaviour
 {
     //variable for restricton
-    private float positiveBound = 30;
-    private float negativeBound = -6;
+    private float positiveBoundZ = 30;
+    private float negativeBoundZ = -6;
+    private float BoundX = 25;
 
     private void Update() {
         //check if the object should be destroyed
-        if (transform.position.z > positiveBound) {
+        if (transform.position.z > positiveBoundZ || transform.position.x > BoundX || transform.position.x < -BoundX) {
             Destroy(gameObject);
-        } else if (transform.position.z < negativeBound) { 
+        } else if (transform.position.z < negativeBoundZ) { 
             Destroy(gameObject);
             Debug.Log("Game over");
         }
