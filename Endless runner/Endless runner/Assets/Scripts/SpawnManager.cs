bb859cc271dcd8886195ad1d5f2680a8f0f81e6f
professorVerbrugghe <lotrdetailsys@gmail.com>
2023-12-31 17:16:55 +0100

    Interval

--- Endless runner/Endless runner/Assets/Scripts/SpawnManager.cs
+++ Endless runner/Endless runner/Assets/Scripts/SpawnManager.cs
@@ -8,7 +8,14 @@ public class SpawnManager : MonoBehaviour {
     [SerializeField] private GameObject obstaclePrefab;
     [SerializeField] private Vector3 spawnPosition;
 
+    private float startTime = 2.0f;
+    private float repeatRate = 2;
+
     private void Start() {
+        InvokeRepeating("SpawnObstacle", startTime, repeatRate);
+    }
+
+    private void SpawnObstacle() {
         Instantiate(obstaclePrefab, spawnPosition, obstaclePrefab.transform.rotation);
     }
 }
