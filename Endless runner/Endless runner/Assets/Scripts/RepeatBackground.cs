25ba31441ec35f647f6d6f47ac93bd8f4b1588c1
professorVerbrugghe <lotrdetailsys@gmail.com>
2024-01-08 11:04:50 +0100

    Fixed background
    Added a box collider to the background and calculating its middle point in the script

--- Endless runner/Endless runner/Assets/Scripts/RepeatBackground.cs
+++ Endless runner/Endless runner/Assets/Scripts/RepeatBackground.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class RepeatBackground : MonoBehaviour {
 
     private Vector3 startPos;
+    private float repeatRate;
 
     private void Start() {
         startPos = transform.position;
+        repeatRate = GetComponent<BoxCollider>().size.x / 2;
     }
 
     private void Update() {
-        if (transform.position.x < startPos.x -50)
+        if (transform.position.x < startPos.x - repeatRate)
         {
             transform.position = startPos;
         }
