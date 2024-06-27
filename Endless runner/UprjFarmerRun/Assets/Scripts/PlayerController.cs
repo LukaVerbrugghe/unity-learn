ab9616dcf151bb718b365c5bfa85b50d0ddf04b8
professorVerbrugghe <lotrdetailsys@gmail.com>
2024-06-27 17:51:07 +0200

    Added jump control

---
+++ Endless runner/UprjFarmerRun/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //variabel for rigidbody
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+        }
+    }
+}
