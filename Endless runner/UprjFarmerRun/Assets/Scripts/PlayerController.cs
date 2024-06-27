07c13bc2c66029dcd521cf8d8d071735ef31707a
professorVerbrugghe <lotrdetailsys@gmail.com>
2024-06-27 18:04:15 +0200

    More precise jumping
    Added a variable for the gravity and for the jump force
    Made sure the player can't jump while in the air

--- Endless runner/UprjFarmerRun/Assets/Scripts/PlayerController.cs
+++ Endless runner/UprjFarmerRun/Assets/Scripts/PlayerController.cs
@@ -4,21 +4,32 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    //variabel for rigidbody
+    //variable for rigidbody
     private Rigidbody rb;
+    //variables for jumping
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float gravity;
+    private bool isJumping = false;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravity;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
         {
-            rb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isJumping = true;
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isJumping = false;
     }
 }
