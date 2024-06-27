4a7f9e7547bb4a1fb4335a4c9cb67b5725697155
professorVerbrugghe <lotrdetailsys@gmail.com>
2024-06-27 19:05:17 +0200

    Obstacles spawn at intervals

--- Endless runner/UprjFarmerRun/Assets/Scripts/SpawnManager.cs
+++ Endless runner/UprjFarmerRun/Assets/Scripts/SpawnManager.cs
@@ -5,17 +5,24 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     [SerializeField] private GameObject m_Spawn;
-    private Vector3 m_SpawnPosition = new Vector3(19, 2.384186e-07f, 9);
+    private Vector3 m_SpawnPosition = new Vector3(10, 2.384186e-07f, 9);
+    [SerializeField] private float startDelay = 2;
+    [SerializeField] private float repeatRate = 2;
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(m_Spawn, m_SpawnPosition, m_Spawn.transform.rotation);
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SpawnObstacle()
+    {
+        Instantiate(m_Spawn, m_SpawnPosition, m_Spawn.transform.rotation);
     }
 }
