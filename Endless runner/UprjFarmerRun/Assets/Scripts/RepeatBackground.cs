f5ed634a0a66cf087db2c67adb07b173c85789b1
professorVerbrugghe <lotrdetailsys@gmail.com>
2024-06-28 08:44:07 +0200

    Seemless background loop

--- Endless runner/UprjFarmerRun/Assets/Scripts/RepeatBackground.cs
+++ Endless runner/UprjFarmerRun/Assets/Scripts/RepeatBackground.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class RepeatBackground : MonoBehaviour
 {
     [SerializeField] private float offset;
+    private Vector3 startPos;
+    private float repeatWidth;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPos = transform.position;
+        repeatWidth = GetComponent<BoxCollider>().size.x / 2;
+        offset = repeatWidth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.x > startPos.x + offset)
+        {
+            transform.position = startPos;
+        }
     }
 }
